17c3fded0e8f52d8d576c7213093016246dbe674
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-05-03 22:19:14 +0300

    Step 10: Move getCharge() from Movie to Price

--- src/main/java/com/fmdev/refactoring/Movie.java
+++ src/main/java/com/fmdev/refactoring/Movie.java
@@ -21,28 +21,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
-        // В зависимости от типа фильма, начисляем заданную стоимость за аренду
-        switch (getFilmType()) {
-            case REGULAR:
-                result = 2;
-                if (daysRented > 2) {
-                    result += (daysRented - 2) * 1.5;
-                }
-                break;
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case CHILDRENS:
-                result = 1.5;
-                if (daysRented > 3) {
-                    result += (daysRented - 3) * 1.5;
-                }
-                break;
-            default:
-        }
-
-        return result;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
--- src/main/java/com/fmdev/refactoring/Price.java
+++ src/main/java/com/fmdev/refactoring/Price.java
@@ -5,4 +5,29 @@ package com.fmdev.refactoring;
  */
 public abstract class Price {
     public abstract FilmType getFilmType();
+
+    public double getCharge(int daysRented) {
+        double result = 0;
+        // В зависимости от типа фильма, начисляем заданную стоимость за аренду
+        switch (getFilmType()) {
+            case REGULAR:
+                result = 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDRENS:
+                result = 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+            default:
+        }
+
+        return result;
+    }
 }
